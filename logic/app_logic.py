9d2067d98e5ef2d8f77a22ed3099f45835d1ee1d
Jakub Komarek <komaja@email.cz>
2024-11-23 17:09:47 +0100

    Docs

--- logic/app_logic.py
+++ logic/app_logic.py
@@ -1,23 +1,44 @@
+"""This module contains the application business logic. It is separate
+from the presentation layer (Flask) and the data storage layer
+to allow for easy testing.
+
+The data storage classes are provided using the dependency injection pattern.
+"""
+
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
+from typing import Optional
 
 from persistence.user_storage import UserStorage
 from persistence.task_database import TaskDatabase
 
 class BadUserException(Exception):
+    """Raised when an invalid user id is provided."""
     pass
 
 class AlreadyDoneException(Exception):
+    """Raised when attempting to complete a task that is already done."""
+    pass
+
+class ParentCantDoTasksException(Exception):
+    """Raised when a parent tries to complete a task."""
     pass
 
 class TaskStatus(str, Enum):
+    """Status of a single task.
+    
+    ACTIVE: The task is available for completion.
+    PAID: The task has been completed and immediately paid for.
+    WAITING: The task has been completed, but selected for delayed payment.
+    """
     ACTIVE = "active"
     PAID = "paid"
     WAITING = "waiting"
 
 @dataclass
 class Task:
+    """Represents a single task."""
     id: int
     task_name: str
     description: str 
@@ -25,12 +46,13 @@ class Task:
     difficulty_level: str
     created_at: datetime 
     is_done: bool
-    done_by: int
+    done_by: Optional[int]  # user_id of the user who completed the task
     done_at: datetime
     waiting: bool
 
     @classmethod
     def load_from_dict(cls, data: dict):
+        """Loads a task from representation on the persistance layer."""
         return cls(
             id=data["task_id"],
             task_name=data["job_name"],
@@ -45,6 +67,7 @@ class Task:
         )
 
     def status(self) -> TaskStatus:
+        """Computes the comletion status of the task."""
         if not self.is_done:
             return TaskStatus.ACTIVE
         if self.waiting:
@@ -54,11 +77,17 @@ class Task:
 
 
 class AppLogic:
+    """Business logic of the application.
+    
+    For now, this is simple enough to be contained in a single class.
+    Persistence-layer entities are passed in the constructor (dependency injection).
+    """
     def __init__(self, user_storage: UserStorage, task_db: TaskDatabase):
         self.user_storage = user_storage
         self.task_db = task_db
 
     def get_user_data(self, user_id: int):
+        """Gets the user information of a user with given id."""
         user = self.user_storage.get_user_by_id(user_id)
         if user is None:
             raise BadUserException()
@@ -66,24 +95,29 @@ class AppLogic:
         return user
 
     def get_all_tasks(self):
+        """Gets the list of all tasks."""
         return [Task.load_from_dict(t) for t in self.task_db.get_all_tasks()]
     
     def get_active_tasks(self):
+        """Gets the list of all active tasks (available to be completed)."""
         return [Task.load_from_dict(t) for t in self.task_db.get_active_tasks()]
     
     def get_tasks_done_by_user(self, user_id: int):
+        """Gets the list of all tasks completed by a user with given id."""
         all_tasks = [Task.load_from_dict(t) for t in self.task_db.get_all_tasks()]
         done_by_user_tasks = [task for task in all_tasks if task.done_by == user_id]
         return done_by_user_tasks
 
     def do_task(self, task_id: int, user_id: int, pay_now: bool):
+        """Processes task completion of a task with given id by user with given id."""
         user = self.user_storage.get_user_by_id(user_id)
         if user is None:
             raise BadUserException()
 
         if user.role == "parent":
-            raise RuntimeError("Parent can't do tasks")
+            raise ParentCantDoTasksException()
 
+        # Get the task, modify and store it back
         task = self.task_db.get_task(task_id)
         if task["is_done"]:
             raise AlreadyDoneException()
