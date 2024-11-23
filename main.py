fd0335c3731ad40aba992ee3989e9d2126f5afa1
Jakub Komarek <komaja@email.cz>
2024-11-23 11:09:32 +0100

    User data endpoint

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import copy
 import json
 from dataclasses import dataclass
 from enum import Enum
+from typing import Optional
 
 from flask import Flask, request
 
-class Role(Enum):
-    PARENT = 1
-    CHILD = 2
+class Role(str, Enum):
+    PARENT = "parent"
+    CHILD = "child"
 
 @dataclass
 class UserData:
@@ -19,8 +21,11 @@ class UserStorage:
     def __init__(self):
         self.db = {}
 
-    def get_user_by_id(self, user_id: int) -> UserData:
-        return self.db.get(user_id).copy()
+    def get_user_by_id(self, user_id: int) -> Optional[UserData]:
+        user = self.db.get(user_id)
+        if user is None:
+            return None
+        return copy.copy(user)
 
     def update_user(self, user: UserData) -> None:
         self.db[user.id] = user
@@ -34,6 +39,15 @@ user_store.add_user(UserData(id=1, name='Alice', balance=100, role=Role.PARENT))
 user_store.add_user(UserData(id=2, name='Bob', balance=200, role=Role.CHILD))
 
 app = Flask(__name__)
+
+@app.route('/user_data/<id>', methods=['GET'])
+def user_data_endpoint(id: str):
+    id = int(id)
+    user = user_store.get_user_by_id(id)
+    if user is None:
+        return 'User not found', 404
+
+    return json.dumps(user.__dict__)
 
 @app.route('/dummy', methods=['PUT'])
 def dummy():
